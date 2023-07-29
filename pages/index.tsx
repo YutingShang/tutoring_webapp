e26c98af4a4a04d414117cb0fe6d504ffc926f50
YutingShang <18shang.yt@gmail.com>
2023-07-29 18:33:16 +0100

    initial template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
     return (
         <>
-            <h1>Hello world!</h1>
+            <h1>Hello World!</h1>
             <p>This is a starter template you can use for Web Dev for Makers projects.</p>
             <img src="/logo.png" alt="Web Dev for Makers Logo" />
         </>
