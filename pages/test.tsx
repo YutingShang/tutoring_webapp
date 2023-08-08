93c3e026ca88a63d8803ae8049a9d4b7665a407f
Yuting Shang <18shang.yt@gmail.com>
2023-08-08 12:25:42 +0100

    basic personal website with css

---
+++ pages/test.tsx
@@ -0,0 +1,15 @@
+export default function Index() {
+    return (
+        <>
+            <div id = "container">
+              
+                <h1>Hello World!</h1>
+                <p>This is a second page.</p>
+                <a href = "/">Back</a>
+                <img src="/logo.png" alt="Web Dev for Makers Logo" />
+
+            </div>
+
+        </>
+    );
+}
