48dc2451cccd14bd86a84f86c2f87cd3ae91148d
Yuting Shang <18shang.yt@gmail.com>
2023-08-10 18:41:48 +0100

    Update content and made profile picture mobile responsive, added head

---
+++ pages/hobbies.tsx
@@ -0,0 +1,15 @@
+export default function Index() {
+    return (
+        <>
+            <div id = "container">
+              
+                <h1>Hello World!</h1>
+                <p>This is the hobbies page.</p>
+                <a href = "/">Back</a>
+           
+
+            </div>
+
+        </>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,75 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCertificate } from '@fortawesome/free-solid-svg-icons';
+import Head from "next/head"
 
 export default function Index() {
     return (
-        <>
-            <div id="nav-bar"><span>Xinqi Shang</span></div>
+        <><Head>
+            <title>Xinqi</title>
+            <meta name="description" content="Xinqi personal tutoring website"/>
+            
+    
+
+        </Head>
+            <div id="nav-bar"><span>Xinqi</span></div>
             <div id="container">
                 <div id="top-section">
-                    
+
                     <button type="button" id="menu-button">Menu</button>
-                    <img src="/chailey-leavers.jpg" id="prof-pic" />
+
+                    <img src="/profile-pic.jpeg" id="prof-pic" />
+
                     <h1>Hi, nice to meet you!<br />I'm Xinqi.</h1>
                 </div>
 
-                {/* <a href = "/test">Link to another page</a>
-                <h1>Hello World!</h1>
-                <p>This is a starter template you can use for Web Dev for Makers projects.</p>
-                <img src="/logo.png" alt="Web Dev for Makers Logo" /> */}
+              
                 <p>
-                    I'm a Natural science graduate from the <b>University of Cambridge</b> where I studied physical natural sciences specialising in material science in my final year
+                    I'm a Natural Sciences graduate from the <b>University of Cambridge</b> where I studied Physical Natural Sciences specialising in Materials Science in my final year.
                 </p>
                 <p>
-                    After graduating, I spent the covid years tutoring online for various subjects including maths, phyiscs and chemistry for a range of students, primarily at the GCSE to ALevel stages.
+                    After graduating, I started tutoring online during lockdown on various subjects including Maths, Further Maths, Phyiscs and Chemistry to a range of students, primarily to the GCSE and A-Level cohort.
                 </p>
                 <p>
-                    In September, I will start my new job as a math teacher at <a href="https://www.lancingcollege.co.uk/"><b>Lancing College</b></a>,  and I will also be dedicating half of my time to the <b>PGCE</b> course
+                    In September, I will be starting my new job as a Maths teacher at <a href="https://www.lancingcollege.co.uk/"><b>Lancing College</b></a>,  and I will also be dedicating half of my time to study towards the <b>PGCE</b> qualification
                 </p>
 
                 <hr />
-                <a href="https://peppapigworld.co.uk/pages/george-pig" className="student-wrapper">
-                    <div className="student-box">
-                    <FontAwesomeIcon className="badge" icon={faCertificate} size='2xl' style={{ color: "#f6d609", }} />
-                        <img src="/andy.png" alt="a pink cartoon pig on a green background" />
 
-                        <h2>Andy 
-                            
-                
-                        </h2>
-                        <p>Feedback "Xinqi helped me start the anti-crime gang at school"</p>
-                    </div>
-                </a>
 
-                <a href="https://peppapigworld.co.uk/pages/danny-dog" className="student-wrapper">
-                    <div className="student-box">
-                        <FontAwesomeIcon className="badge" icon={faCertificate} size='2xl' style={{ color: "#f6d609", }} />
-                        <img src="/darren.png" alt="a brown cartoon dog on a green background" />
-                        <h2>Darren</h2>
-                        <p>Feedback "Xinqi told me to buy some wires, so I bought them off taobao. She hasn't told me what to do with them yet"</p>
-                    </div>
-                </a>
-             
+                <div id="flex-container">
+                    <a href="/feedback" className="content-wrapper">
+                        <div className="content-box">
+
+                            <img src="/chat-bubbles.jpeg" alt="white sticky notes with blue speech marks " />
+
+                            <h2>Student <br/> Reviews
+                            </h2>
+                            <p>Helping students gain the grades they aspire</p>
+
+                        </div>
+                    </a>
+
+                    <a href="/teaching" className="content-wrapper">
+                        <div className="content-box">
+
+                            <img src="/ideas.jpeg" alt="a blackboard with chalk drawings of ideas radiating out from a light bulb in the middle" />
+                            <h2>Teaching methodology</h2>
+                            <p>Tailored to students of individual abilities</p>
+                        </div>
+                    </a>
+
+                    <a href="/hobbies" className="content-wrapper">
+                        <div className="content-box">
+
+                            <img src="/crochet-green.png" alt="a pair of dangling cherry crochet earings against a matcha green background with a reel of string and crochet hook in the foreground " />
+                            <h2>Hobbies and interests</h2>
+                            <p>Hands on crafts with overloaded <i>cuteness</i></p>
+
+                        </div>
+                    </a>
+
+                </div>
+
 
             </div>
 
