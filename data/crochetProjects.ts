5165e6c256a70d62ba2b26a8e1f89160d90a7a91
Yuting Shang <18shang.yt@gmail.com>
2023-08-12 21:30:51 +0100

    added dynamic pages to the hobbies page to create new info pages for each crochet project

---
+++ data/crochetProjects.ts
@@ -0,0 +1,30 @@
+const crochetArray = [
+    {
+        name: "tiger",
+        title : "Tiger keychain",
+        imageUrl: "/tiger.jpeg",
+        description : "A cute orange micro-crochet tiger I made for myself. I added a crochet heart and made it into a keychain. "
+    },
+    {
+        name: "mice1",
+        title : "Mice and Melon",
+        imageUrl: "/mice-melon.jpeg",
+        description : "Tiny micro-crochet doormice in a lovely lavender color, clutching onto a fresh slice of watermelon"
+    },
+    {
+        name: "mice2",
+        title : "Easy peasy lemon squeezy",
+        imageUrl: "/mice-lemon.jpeg",
+        description : "The cute purple mice is now holding onto a micro-crochet lemon!"
+    },
+    {
+        name: "heart",
+        title : "Heart earings",
+        imageUrl: "/heart.jpeg",
+        description : "One dangling red heart earing I made"
+    },
+
+
+]
+
+export default crochetArray;
--- pages/hobbies.tsx
+++ pages/hobbies.tsx
@@ -1,4 +1,7 @@
+import crochetArray from "../data/crochetProjects";
+
 export default function Index() {
+
     return (
         <>
             <div id = "container">
@@ -6,8 +9,11 @@ export default function Index() {
                 <h1>Hello World!</h1>
                 <p>This is the hobbies page.</p>
                 <a href = "/">Back</a>
-           
-
+           <p>Here is a list of my micro-crochet projects:</p>
+           <ul>
+            
+           </ul>
+{crochetArray.map(proj => <li><a href={proj.name}>{proj.title}</a></li>)}
             </div>
 
         </>
