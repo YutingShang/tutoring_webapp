d4109a4585070e71b54a4b1bd49ff56f89b67963
Yuting Shang <18shang.yt@gmail.com>
2023-08-30 15:25:59 +0100

    differentiate between loading and empty database on admin and feedback page

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -6,9 +6,9 @@ import LoadingIcons from 'react-loading-icons'
 
 
 export default function Admin() {
-    const [revsArray, setRevsArray] = useState<{ _id: string, review: { original: string, current: string }, name: { original: string, current: string }, level: { original: string, current: string }, subject: { original: string, current: string }, displayed: boolean }[]>([]);
+    const [revsArray, setRevsArray] = useState<{ _id: string, review: { original: string, current: string }, name: { original: string, current: string }, level: { original: string, current: string }, subject: { original: string, current: string }, displayed: boolean }[] | null>(null);
     //dont really need name, subject and level on this page
-    
+
     function onRequest() {
         axios.get("/api/review")
             .then(res => { setRevsArray(res.data.reviewsArray) }).catch(e => console.log(e))     //gets the new data and then updates the local array
@@ -32,8 +32,8 @@ export default function Admin() {
     function onToggleDisplay(id: string, displayed: boolean) {
 
         //update the ui first, then update on server
-        let revsArrayCopy = [...revsArray]
-        let reviewItem = revsArrayCopy.find(r=>r._id==id)
+        let revsArrayCopy = [...revsArray!]
+        let reviewItem = revsArrayCopy.find(r => r._id == id)
         reviewItem!.displayed = !reviewItem!.displayed
         setRevsArray(revsArrayCopy)
 
@@ -58,25 +58,27 @@ export default function Admin() {
                 <div className="text-center">
 
 
-                    {revsArray.length!=0? 
-                    (revsArray.map((r) =>     //!!each should have a unique key prop
+                    {revsArray ?    //whether it has been loaded yet, if not show loading icon
+                        //whether the database is empty
+                        (revsArray.length == 0 ? <p className="text-[#b8bab8]">No reviews yet</p> : 
+                            (revsArray.map((r) =>     //!!each should have a unique key prop
 
-                        //the blue button cards
-                        <div key={r._id} className={"inline-block relative  m-4 rounded-lg " + (r.displayed ? "bg-sky-200  drop-shadow-md" : "bg-white  drop-shadow-md bg-slate-50")}>
-                            <button className={"reviewButton inline-block p-5 mr-14"}
-                                onClick={() => onToggleDisplay(r._id, r.displayed)}>
+                                //the blue button cards
+                                <div key={r._id} className={"inline-block relative  m-4 rounded-lg " + (r.displayed ? "bg-sky-200  drop-shadow-md" : "bg-white  drop-shadow-md bg-slate-50")}>
+                                    <button className={"reviewButton inline-block p-5 mr-14"}
+                                        onClick={() => onToggleDisplay(r._id, r.displayed)}>
 
-                                <p className="text-[16px] leading-[24px]">{r.review.current ? r.review.current : r.review.original}</p>    {/*shows original if 'current' edit doesnt exist*/}
-                            </button>
-                            <a href={'/admin/' + r._id}>
-                                <FontAwesomeIcon className="absolute block top-[50%] translate-y-[-50%] right-4" icon={faPenToSquare} size="lg" style={{ color: "#0369A1" }} />
+                                        <p className="text-[16px] leading-[24px]">{r.review.current ? r.review.current : r.review.original}</p>    {/*shows original if 'current' edit doesnt exist*/}
+                                    </button>
+                                    <a href={'/admin/' + r._id}>
+                                        <FontAwesomeIcon className="absolute block top-[50%] translate-y-[-50%] right-4" icon={faPenToSquare} size="lg" style={{ color: "#0369A1" }} />
 
-                            </a>
+                                    </a>
 
-                        </div>
+                                </div>
 
-                    )): 
-                    <LoadingIcons.TailSpin className="mx-auto block mt-[32px] w-6" stroke="#0369A1" strokeOpacity={1} />
+                            )) ) :
+                        <LoadingIcons.TailSpin className="mx-auto block mt-[32px] w-6" stroke="#0369A1" strokeOpacity={1} />
                     }
 
                 </div>
--- pages/feedback.tsx
+++ pages/feedback.tsx
@@ -1,11 +1,12 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 // import reviewsArray from "../data/reviews";
 import ReviewBubble from "../components/ReviewBubble";
 import axios from "axios"
 import HamburgerMenu from "../components/HamburgerMenu";
+import LoadingIcons from "react-loading-icons";
 
 export default function Feedback() {
-    const [search,setSearch] = useState("");
+    const [search, setSearch] = useState("");
     // const filteredReviewsArray = reviewsArray.filter(r=> r.text.toLowerCase().includes(search.toLowerCase()) ||
     // r.subject.toLowerCase().includes(search.toLowerCase()) ||
     // r.date.toLowerCase().includes(search.toLowerCase()) ||
@@ -13,8 +14,8 @@ export default function Feedback() {
 
     //pulling data fromt the database
 
-    const [revsArray, setRevsArray] = useState<{ _id: string, review: {original: string, current:string}, name: {original: string, current:string}, level: {original: string, current:string}, subject: {original: string, current:string}, displayed: boolean, date:string, examBoard:string }[]>([]);
-   // console.log(revsArray)
+    const [revsArray, setRevsArray] = useState<{ _id: string, review: { original: string, current: string }, name: { original: string, current: string }, level: { original: string, current: string }, subject: { original: string, current: string }, displayed: boolean, date: string, examBoard: string }[] | null>(null);
+    // console.log(revsArray)
     function onRequest() {
         axios.get("/api/review")
             .then(res => { setRevsArray(res.data.reviewsArray) }).catch(e => console.log(e))     //gets the new data and then updates the local array
@@ -33,48 +34,52 @@ export default function Feedback() {
         return () => clearInterval(interval)
     }, [onRequest])
 
-    
 
-    //new filtered array
-    const filteredReviewsArray = revsArray.filter(r=> 
-    (r.displayed) &&     //must be set to 'displayed' in admin AND one of the below (i.e. contains a searched phrase if there is a search)
-    (
-    (r.review.current??r.review.original).toLowerCase().includes(search.toLowerCase()) ||
-    (r.subject.current??r.subject.original).toLowerCase().includes(search.toLowerCase())  ||
-    ((r.date? true: false) && r.date.toLowerCase().includes(search.toLowerCase())) ||
-    (r.level.current?? r.level.original).toLowerCase().includes(search.toLowerCase())   
+
+    //new filtered array - could be null
+    const filteredReviewsArray = revsArray?.filter(r =>
+        (r.displayed) &&     //must be set to 'displayed' in admin AND one of the below (i.e. contains a searched phrase if there is a search)
+        (
+            (r.review.current ?? r.review.original).toLowerCase().includes(search.toLowerCase()) ||
+            (r.subject.current ?? r.subject.original).toLowerCase().includes(search.toLowerCase()) ||
+            ((r.date ? true : false) && r.date.toLowerCase().includes(search.toLowerCase())) ||
+            (r.level.current ?? r.level.original).toLowerCase().includes(search.toLowerCase())
+        )
     )
-    )
-    
-  
+
+
     return (
         <>
-                <HamburgerMenu home aboutMe leaveReview admin/>
-              
-                <div className="nav-bar"><span>Reviews</span></div>
-                <div className = "container">
+            <HamburgerMenu home aboutMe leaveReview admin />
+
+            <div className="nav-bar"><span>Reviews</span></div>
+            <div className="container">
                 <div className="top-section">
                     <a href="/"><img src="/profile-pic.jpeg" id="home-circle" /></a>
-                        <p className="intro">Take a look at the feedback I have recieved. <br></br>Search for keywords such as subject or date</p>
-                    </div>
+                    <p className="intro">Take a look at the feedback I have recieved. <br></br>Search for keywords such as subject or date</p>
+                </div>
 
-                    <div className="reivew-section">
-            
-                        <input  className="search-bar" type="text" placeholder="Type..." value={search} onChange={e=>setSearch(e.target.value)} />
+                <div className="reivew-section">
+
+                    <input className="search-bar" type="text" placeholder="Type..." value={search} onChange={e => setSearch(e.target.value)} />
                     <i></i>
-                   
-              
-             { (filteredReviewsArray.length>0)?
-             
-             (<>{filteredReviewsArray.map((rev, index)=> 
-                (index %2!=0)?
-                
-                (<ReviewBubble key={rev._id} direction="right" text={rev.review.current?? rev.review.original} level={rev.level.current?? rev.level.original} date={rev.date} student={rev.name.current??rev.name.original} subject={rev.subject.current??rev.subject.original}/>) : 
-                (<ReviewBubble key={rev._id} direction="left" text={rev.review.current?? rev.review.original} level={rev.level.current?? rev.level.original} date={rev.date} student={rev.name.current??rev.name.original} subject={rev.subject.current??rev.subject.original}/>) )}</>)
-                :
-                (<p id="no-results">No search results</p>)
-             }  
-</div>
+
+
+                    {filteredReviewsArray ? (
+                        (filteredReviewsArray.length > 0) ?
+
+                            (<>{filteredReviewsArray.map((rev, index) =>
+                                (index % 2 != 0) ?
+
+                                    (<ReviewBubble key={rev._id} direction="right" text={rev.review.current ?? rev.review.original} level={rev.level.current ?? rev.level.original} date={rev.date} student={rev.name.current ?? rev.name.original} subject={rev.subject.current ?? rev.subject.original} />) :
+                                    (<ReviewBubble key={rev._id} direction="left" text={rev.review.current ?? rev.review.original} level={rev.level.current ?? rev.level.original} date={rev.date} student={rev.name.current ?? rev.name.original} subject={rev.subject.current ?? rev.subject.original} />))}</>)
+                            :
+                            (<p id="no-results">No search results</p>) )
+                        :
+                        // Loading if not retrieved from database yet
+                        <LoadingIcons.TailSpin className="mx-auto block mt-[32px] w-6" stroke="#0369A1" strokeOpacity={1} />
+                    }
+                </div>
             </div>
 
         </>
